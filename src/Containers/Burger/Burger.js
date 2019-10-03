3c281a67ea623526c8e373d125db3a59bcae0430
chris13012 <christophecaillet@icloud.com>
2019-10-03 11:42:43 +0200

    first commit

---
+++ src/Containers/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Ingredients from "../Ingredients/Ingredients";
+import "./Burger.css";
+import { connect } from "react-redux";
+
+const Burger = props => {
+  return (
+    <div className="Burger" style={{ marginTop: 35 }}>
+      <Ingredients type="BreadTop" />
+      {ingredients.length === 0 ? (
+        <p>Selectionnez au moins un ingredient</p>
+      ) : (
+        ingredients.map((igd, index) => {
+          return <Ingredients key={igd + index} type={igd} />;
+        })
+      )}
+      <Ingredients type="BreadBottom" />
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    ingredients: state.ingredients
+  };
+};
+
+export default connect(mapStateToProps)(Burger);
