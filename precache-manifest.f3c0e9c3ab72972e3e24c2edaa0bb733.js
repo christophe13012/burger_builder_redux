0e6f5e9d08bfcd03fb60a6996b75f5990fb50d73
chris13012 <christophecaillet@icloud.com>
2019-10-16 11:53:41 +0200

    Updates

---
+++ precache-manifest.f3c0e9c3ab72972e3e24c2edaa0bb733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196f1a9281b5f0db8b7e5eab612c73fd",
+    "url": "/burger_builder_redux/index.html"
+  },
+  {
+    "revision": "1ce2142696a9a710536d",
+    "url": "/burger_builder_redux/static/css/2.d6da3c05.chunk.css"
+  },
+  {
+    "revision": "e77500e5399c7a1485a8",
+    "url": "/burger_builder_redux/static/css/main.cbcabb4d.chunk.css"
+  },
+  {
+    "revision": "1ce2142696a9a710536d",
+    "url": "/burger_builder_redux/static/js/2.a63c8774.chunk.js"
+  },
+  {
+    "revision": "e77500e5399c7a1485a8",
+    "url": "/burger_builder_redux/static/js/main.140c16ca.chunk.js"
+  },
+  {
+    "revision": "f05f8ba99de4630237a5",
+    "url": "/burger_builder_redux/static/js/runtime-main.25901cbd.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder_redux/static/media/burger-logo.b8503d26.png"
+  }
+]);
